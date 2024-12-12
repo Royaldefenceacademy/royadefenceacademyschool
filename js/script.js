77c8bc5d82cf4187d84f874a8968375e81b0f33d
krishna soni <soni01krishna@gmail.com>
2024-12-12 18:26:02 +0530

    make nav fixed

--- js/script.js
+++ js/script.js
@@ -36,4 +36,22 @@ async function fetchSliderImages() {
 // Initialize the carousel with fetched images
 fetchSliderImages();
  
+window.addEventListener('scroll',scrollONevent)
 
+function scrollONevent (){
+    let navigation = document.getElementById('nav')
+    let content = document.getElementById('content')
+    let screen = window.scrollY
+    if(screen > 0 ){
+        navigation.style.position = "fixed";
+        navigation.style.width="100%"
+        content.style.display="none"
+        console.log(navigation.style.position)
+    }
+    else{
+        navigation.style.position = "static";
+        navigation.style.width="100%"
+        content.style.display="block"
+        console.log(navigation.style.position)
+    }
+}
