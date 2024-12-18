38b4aa1a62addf0a570dc4495b1cc4a323420631
krishna soni <soni01krishna@gmail.com>
2024-12-18 13:53:26 +0530

    responsive header doem

--- js/script.js
+++ js/script.js
@@ -7,17 +7,21 @@ function scrollONevent (){
     let navigation = document.getElementById('nav')
     let content = document.getElementById('content')
     let screen = window.scrollY
-    if(screen > 0 ){
-        navigation.style.position = "fixed";
-        navigation.style.width="100%"
-        content.style.display="none"
-        console.log(navigation.style.position)
+    let width = window.screen.availWidth;
+    if(width > 700){
+        if(screen > 0 ){
+            navigation.style.position = "fixed";
+            navigation.style.width="100%"
+            content.style.display="none"
+            console.log(navigation.style.position)
+        }
+        else{
+            navigation.style.position = "static";
+            navigation.style.width="100%"
+            content.style.display="block"
+            // console.log(navigation.style.position)
+        }
     }
-    else{
-        navigation.style.position = "static";
-        navigation.style.width="100%"
-        content.style.display="block"
-        // console.log(navigation.style.position)
-    }
+    
 }
 
