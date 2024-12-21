9ea1b1cc6938127480dcb944f210758d7437f901
krishna soni <110759309+krishnaSONI011@users.noreply.github.com>
2024-12-21 14:46:08 +0530

    a

--- js/selectedStudentImg.js
+++ js/selectedStudentImg.js
@@ -4,15 +4,6 @@ let insetGallery = document.getElementById('insertStudent');
 let imageLinkArray = ['all india sainik school entrance exam aissee 2022 Abhishek Kumar  Royal Defecne Academy.png','all india sainik school entrance exam aissee 2022 anirudha singh Royal Defecne Academy.png','all india sainik school entrance exam aissee 2022 chanchal singh Royal Academy.png','all india sainik school entrance exam aissee 2022 chatanya mishra Royal Defecne Academy.png','all india sainik school entrance exam aissee 2022 dishant patil Royal Defecne Academy.png','all india sainik school entrance exam aissee 2022 Divyanshu  . Royal Defecne Academy.png','all india sainik school entrance exam aissee 2022 harendra kumar.png','all india sainik school entrance exam aissee 2022 KARTIK  . Royal Defecne Academy.png','all india sainik school entrance exam aissee 2022 Divyanshu  . Royal Defecne Academy.png','all india sainik school entrance exam aissee 2022 prabhat kumar  Royal Defecne Academy.png.jpg','all india sainik school entrance exam aissee 2022 prakash singh . Royal Defecne Academy.png','all india sainik school entrance exam aissee 2022 raghvendra raj Royal Defecne Academy.png','all india sainik school entrance exam aissee 2022 Rishabh verma . Royal Defecne Academy.png','all india sainik school entrance exam aissee 2022 saina patil.png','all india sainik school entrance exam aissee 2022 vansh sharma royal academy.png','all india sainik school entrance exam aissee 2022 vishwajeet singh rathore  Royal Defecne Academy.png','all india sainik school entrance exam aissee 2022 yuvraj jadon  Royal Defecne Academy.png','all india sainik school entrance exam aissee 2022 vansh sharma royal academy.png']
 let loadedImages = 0;
 
-// imageLinkArray.forEach((src,index)=>{
-//     insetGallery.innerHTML += ` 
-//     <div class="col-md-2 mt-3 ">
-//                     <img src="./images/${src}" alt="${src}" title="${src}" class="img-fluid rounded">
-//                 </div>
-// `}
-
-// )
-
 imageLinkArray.forEach((src, index) => {
     const img = document.createElement('img');
     img.src = `./images/${src}`;
